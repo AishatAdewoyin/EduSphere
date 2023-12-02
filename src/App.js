3752e6d485d2b3f959bcfda44e398b90b2f26ac3
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-12-02 10:39:46 +0100

    structured

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Analytics from './components/Analytics';
+import Cards from './components/Cards';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Newsletter from './components/Newsletter';
 
 function App() {
   return (
     <div>
-      <h1 className='bg-black text-white'>Edusphere</h1>
+      <Navbar />
+      <Hero />
+      <Analytics />
+      <Newsletter />
+      <Cards />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
