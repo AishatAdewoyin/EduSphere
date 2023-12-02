15d91bb72b7907a042febdfcf0c357addf2e5bd6
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-12-02 12:02:15 +0100

    home page done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,25 +6,25 @@ const Hero = () => {
     <div className='text-white'>
       <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
         <p className='text-[#00df9a] font-bold p-2'>
-          GROWING WITH DATA ANALYTICS
+          THE SCHOOL'S SOCIAL HUB
         </p>
-        <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>
-          Grow with data.
+        <h1 className='md:text-6xl sm:text-6xl text-4xl font-bold md:py-6'>
+          Welcome to EduSphere
         </h1>
         <div className='flex justify-center items-center'>
-          <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>
-            Fast, flexible financing for
+          <p className='md:text-4xl sm:text-4xl text-xl font-bold py-4'>
+            Here, We 
           </p>
           <Typed
-          className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'
-            strings={['BTB', 'BTC', 'SASS']}
+          className='md:text-4xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'
+            strings={['Connect.', 'Colaborate.', 'Thrive.']}
             typeSpeed={120}
             backSpeed={140}
             loop
           />
         </div>
-        <p className='md:text-2xl text-xl font-bold text-gray-500'>Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms.</p>
-        <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
+        <p className='md:text-xl text-xl font-bold text-gray-500'>The dynamic social media platform designed exclusively for students and administrators of our school. eduSphere is more than just a virtual space; it's a vibrant community where knowledge meets collaboration, and connections flourish.</p>
+        <a href='../pages/signup.jsx'><button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button></a>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import React, { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -9,25 +9,65 @@ const Navbar = () => {
   };
 
   return (
-    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
-      <ul className='hidden md:flex'>
-        <li className='p-4'>Home</li>
-        <li className='p-4'>Company</li>
-        <li className='p-4'>Resources</li>
-        <li className='p-4'>About</li>
-        <li className='p-4'>Contact</li>
+    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white w-full">
+      <a href="#">
+        <h1 className="text-3xl font-bold text-[#00df9a]">EduSphere</h1>
+      </a>
+      <ul className="hidden md:flex">
+        <li>
+          <a href="#" className="p-4">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="../pages/careers.jsx" className="p-4">
+            Careers
+          </a>
+        </li>
+        <li>
+          <a href="../pages/login.jsx" className="p-4">
+            Profile
+          </a>
+        </li>
+        <li>
+          <a href="../pages/about.jsx" className="p-4">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="../pages/contact.jsx" className="p-4">
+            Contact
+          </a>
+        </li>
       </ul>
-      <div onClick={handleNav} className='block md:hidden'>
-          {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
+      <div onClick={handleNav} className="block md:hidden">
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
-      <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
-          <li className='p-4 border-b border-gray-600'>Home</li>
-          <li className='p-4 border-b border-gray-600'>Company</li>
-          <li className='p-4 border-b border-gray-600'>Resources</li>
-          <li className='p-4 border-b border-gray-600'>About</li>
-          <li className='p-4'>Contact</li>
+      <ul
+        className={
+          nav
+            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
+            : "ease-in-out duration-500 fixed left-[-100%]"
+        }
+      >
+        <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">
+          EduSphere
+        </h1>
+        <li>
+          <a href="#" className="p-4 border-b border-gray-600">Home</a>
+        </li>
+        <li>
+          <a href="../pages/careers.jsx" className="p-4 border-b border-gray-600">Career</a>
+        </li>
+        <li>
+          <a href="../pages/login.jsx" className="p-4 border-b border-gray-600">Profile</a>
+        </li>
+        <li>
+          <a href="../pages/about.jsx" className="p-4 border-b border-gray-600">About</a>
+        </li>
+        <li>
+          <a href="../pages/contact.jsx" className="p-4">Contact</a>
+        </li>
       </ul>
     </div>
   );
