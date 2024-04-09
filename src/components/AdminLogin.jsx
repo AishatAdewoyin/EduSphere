e6d30500ae59c7e6108f02dc61802f3c0aaef6ef
AishatAdewoyin <adewoyinaishat@gmail.com>
2024-04-09 18:04:08 +0100

    admin login updated

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -20,59 +20,47 @@ const AdminLogin = () => {
 
   return (
     <div className="w-full lg:w-5/12 bg-white p-5 font-mono rounded-lg mx-auto">
-      <h3 className="pt-4 text-2xl text-center">
-        Login To Your Account As an Admin!
-      </h3>
+      <h3 className="pt-4 text-2xl text-center">Login To Your Account As an Admin!</h3>
       <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded" onSubmit={adminLogin}>
-        <div className="mb-4 md:flex md:justify-between">
-          <div className="mb-4">
-            <label
-              className="block mb-2 text-sm font-bold text-gray-700"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <input
-              className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-              id="email"
-              type="email"
-              placeholder="Email"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <div className="md:ml-2">
-              <label
-                className="block mb-2 text-sm font-bold text-gray-700"
-                htmlFor="c_password"
-              >
-                Confirm Password
-              </label>
-              <input
-                className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                id="c_password"
-                type="password"
-                placeholder="*********"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-          </div>
-          <div className="mb-6 text-center">
-            <button
-              className="w-full px-4 py-2 font-bold text-black bg-[#00df9a] rounded-full hover:bg-[#47ffc5] focus:outline-none focus:shadow-outline"
-              type="submit"
-            >
-              Sign In
-            </button>
-          </div>
-          <hr className="mb-6 border-t" />
-          <div className="text-center">
-            <Link href="#">
-              <span className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800">
-                Forgot Password?
-              </span>
-            </Link>
-          </div>
+        <div className="mb-4">
+          <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="mb-6 text-center">
+          <button
+            className="w-full px-4 py-2 font-bold text-black bg-[#00df9a] rounded-full hover:bg-[#47ffc5] focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Sign In
+          </button>
+        </div>
+        <hr className="mb-6 border-t" />
+        <div className="text-center">
+          <Link href="#">
+            <span className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800">
+              Forgot Password?
+            </span>
+          </Link>
         </div>
       </form>
     </div>
